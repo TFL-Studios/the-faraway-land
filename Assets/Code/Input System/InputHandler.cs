182c4c8a96a281a031828fbf25ac71f762e6c75f
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2026-03-21 13:53:32 -0300

    input handler qof

--- Assets/Code/Input System/InputHandler.cs
+++ Assets/Code/Input System/InputHandler.cs
@@ -1,69 +1,76 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public interface IEntrada
+{
+    public void RegistrarCallbacks();
+    public void Habilitar();
+    public void Desabilitar();
+}
+
+public class EntradaCustomizada<T> : IEntrada where T : struct
+{
+    private InputAction _acao;
+
+    private T _valor;
+    public T Valor { get { return this._valor; } }
+
+    public bool FoiPressionada { get { return this._acao.WasPressedThisFrame(); } }
+    public bool EstaPressionada { get { return this._acao.IsPressed(); } }
+    public bool FoiSolta { get { return this._acao.WasReleasedThisFrame(); } }
+
+    public EntradaCustomizada(InputAction acao)
+    {
+        this._acao = acao;
+    }
+
+    public void RegistrarCallbacks()
+    {
+        if (typeof(T) == typeof(bool)) return;
+        this._acao.performed += context => this._valor = context.ReadValue<T>();
+        this._acao.canceled += context => this._valor = default(T);
+    }
+
+    public void Habilitar() => this._acao.Enable();
+    public void Desabilitar() => this._acao.Disable();
+}
+
 public class InputHandler : PersistentSingleton<InputHandler>
 {
     [SerializeField] private InputActionAsset _assetDeAcoesDeEntrada;
 
-    private InputAction[] _acoesDeEntrada;
-
-    /* Acoes de Input */
+    /* Entradas Customizadas */
+    private IEntrada[] _entradas;
     // Interface
-    private InputAction _acaoDeEntradaDeAbrirMenu;
-    private InputAction _acaoDeEntradaDeAbrirInventario;
-    private InputAction _acaoDeEntradaDeAbrirDiario;
-    private InputAction _acaoDeEntradaDeNavegar;
-    private InputAction _acaoDeEntradaDeSelecionar;
+    private EntradaCustomizada<bool> _entradaMenu;
+    private EntradaCustomizada<bool> _entradaInventario;
+    private EntradaCustomizada<bool> _entradaDiario;
+    private EntradaCustomizada<Vector2> _entradaNavegacao;
+    private EntradaCustomizada<bool> _entradaSelecao;
     // Jogador
-    private InputAction _acaoDeEntradaDeMover;
-    private InputAction _acaoDeEntradaDeCorrer;
-    private InputAction _acaoDeEntradaDeAgachar;
-    private InputAction _acaoDeEntradaDeInteragir;
-    private InputAction _acaoDeEntradaDeLanterna;
-    private InputAction _acaoDeEntradaDeVisao;
-    private InputAction _acaoDeEntradaDeTrocarPOV;
+    private EntradaCustomizada<Vector2> _entradaMovimento;
+    private EntradaCustomizada<bool> _entradaCorrida;
+    private EntradaCustomizada<bool> _entradaAgachamento;
+    private EntradaCustomizada<bool> _entradaInteracao;
+    private EntradaCustomizada<bool> _entradaLanterna;
+    private EntradaCustomizada<Vector2> _entradaVisao;
+    private EntradaCustomizada<bool> _entradaPOV;
 
-    /* Valores */
+    /* Acesso */
     // Interface
-    private bool _entradaDeAbrirOMenuDaInterface;
-    public bool EntradaDeAbrirOMenuDaInterface { get { return this._entradaDeAbrirOMenuDaInterface; } }
-
-    private bool _entradaDeAbrirOInventarioDaInterface;
-    public bool EntradaDeAbrirOInventarioDaInterface { get { return this._entradaDeAbrirOInventarioDaInterface; } }
-
-    private bool _entradaDeAbrirODiarioDaInterface;
-    public bool EntradaDeAbrirODiarioDaInterface { get { return this._entradaDeAbrirODiarioDaInterface; } }
-
-    private Vector2 _entradaDeNavegacaoDaInterface;
-    public Vector2 EntradaDeNavegacaoDaInterface { get { return this._entradaDeNavegacaoDaInterface; } }
-
-    private bool _entradaDeSelecaoDaInterface;
-    public bool EntradaDeSelecaoDaInterface { get { return this._entradaDeSelecaoDaInterface; } }
-
+    public EntradaCustomizada<bool> EntradaMenu { get { return this._entradaMenu; } }
+    public EntradaCustomizada<bool> EntradaInventario { get { return this._entradaInventario; } }
+    public EntradaCustomizada<bool> EntradaDiario { get { return this._entradaDiario; } }
+    public EntradaCustomizada<Vector2> EntradaNavegacao { get { return this._entradaNavegacao; } }
+    public EntradaCustomizada<bool> EntradaSelecao { get { return this._entradaSelecao; } }
     // Jogador
-    private Vector2 _entradaDeMovimentoDoJogador;
-    public Vector2 EntradaDeMovimentoDoJogador { get { return this._entradaDeMovimentoDoJogador; } }
-
-    private bool _entradaDeCorridaDoJogador;
-    public bool EntradaDeCorridaDoJogador { get { return this._entradaDeCorridaDoJogador; } }
-
-    private bool _entradaDePuloDoJogador;
-    public bool EntradaDePuloDoJogador { get { return this._entradaDePuloDoJogador; } }
-
-    private bool _entradaDeAgacharDoJogador;
-    public bool EntradaDeAgacharDoJogador { get { return this._entradaDeAgacharDoJogador; } }
-
-    private bool _entradaDeInteracaoDoJogador;
-    public bool EntradaDeInteracaoDoJogador { get { return this._entradaDeInteracaoDoJogador; } }
-
-    private bool _entradaDeLanternaDoJogador;
-    public bool EntradaDeLanternaDoJogador { get { return this._entradaDeLanternaDoJogador; } }
-
-    private Vector2 _entradaDeVisaoDoJogador;
-    public Vector2 EntradaDeVisaoDoJogador { get { return this._entradaDeVisaoDoJogador; } }
-
-    private bool _entradaDeTrocaDePOVDoJogador;
-    public bool EntradaDeTrocaDePOVDoJogador { get { return this._entradaDeTrocaDePOVDoJogador; } }
+    public EntradaCustomizada<Vector2> EntradaMovimento { get { return this._entradaMovimento; } }
+    public EntradaCustomizada<bool> EntradaCorrida { get { return this._entradaCorrida; } }
+    public EntradaCustomizada<bool> EntradaAgachamento { get { return this._entradaAgachamento; } }
+    public EntradaCustomizada<bool> EntradaInteracao { get { return this._entradaInteracao; } }
+    public EntradaCustomizada<bool> EntradaLanterna { get { return this._entradaLanterna; } }
+    public EntradaCustomizada<Vector2> EntradaVisao { get { return this._entradaVisao; } }
+    public EntradaCustomizada<bool> EntradaPOV { get { return this._entradaPOV; } }
 
     protected override void Awake()
     {
@@ -72,81 +79,46 @@ public class InputHandler : PersistentSingleton<InputHandler>
         InputActionMap mapaDeAcoesDeEntradaDeInterface = this._assetDeAcoesDeEntrada.FindActionMap("Interface");
         InputActionMap mapaDeAcoesDeEntradaDoJogador = this._assetDeAcoesDeEntrada.FindActionMap("Jogador");
 
-        this._acoesDeEntrada = new InputAction[]
+        this._entradas = new IEntrada[]
         {
-            // Interface
-            this._acaoDeEntradaDeAbrirMenu = mapaDeAcoesDeEntradaDeInterface.FindAction("Menu"),
-            this._acaoDeEntradaDeAbrirInventario = mapaDeAcoesDeEntradaDeInterface.FindAction("Inventario"),
-            this._acaoDeEntradaDeAbrirDiario = mapaDeAcoesDeEntradaDeInterface.FindAction("Diario"),
-            this._acaoDeEntradaDeNavegar = mapaDeAcoesDeEntradaDeInterface.FindAction("Navegar"),
-            this._acaoDeEntradaDeSelecionar = mapaDeAcoesDeEntradaDeInterface.FindAction("Selecionar"),
-            // Jogador
-            this._acaoDeEntradaDeMover = mapaDeAcoesDeEntradaDoJogador.FindAction("Mover"),
-            this._acaoDeEntradaDeCorrer = mapaDeAcoesDeEntradaDoJogador.FindAction("Correr"),
-            this._acaoDeEntradaDeAgachar = mapaDeAcoesDeEntradaDoJogador.FindAction("Agachar"),
-            this._acaoDeEntradaDeInteragir = mapaDeAcoesDeEntradaDoJogador.FindAction("Interagir"),
-            this._acaoDeEntradaDeLanterna = mapaDeAcoesDeEntradaDoJogador.FindAction("Lanterna"),
-            this._acaoDeEntradaDeVisao = mapaDeAcoesDeEntradaDoJogador.FindAction("Visao"),
-            this._acaoDeEntradaDeTrocarPOV = mapaDeAcoesDeEntradaDoJogador.FindAction("TrocarPOV"),
+            this._entradaMenu = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDeInterface.FindAction("Menu")),
+            this._entradaInventario = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDeInterface.FindAction("Inventario")),
+            this._entradaDiario = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDeInterface.FindAction("Diario")),
+            this._entradaNavegacao = new EntradaCustomizada<Vector2>(mapaDeAcoesDeEntradaDeInterface.FindAction("Navegar")),
+            this._entradaSelecao = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDeInterface.FindAction("Selecionar")),
+            this._entradaMovimento = new EntradaCustomizada<Vector2>(mapaDeAcoesDeEntradaDoJogador.FindAction("Mover")),
+            this._entradaCorrida = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDoJogador.FindAction("Correr")),
+            this._entradaAgachamento = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDoJogador.FindAction("Agachar")),
+            this._entradaInteracao = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDoJogador.FindAction("Interagir")),
+            this._entradaLanterna = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDoJogador.FindAction("Lanterna")),
+            this._entradaVisao = new EntradaCustomizada<Vector2>(mapaDeAcoesDeEntradaDoJogador.FindAction("Visao")),
+            this._entradaPOV = new EntradaCustomizada<bool>(mapaDeAcoesDeEntradaDoJogador.FindAction("TrocarPOV"))
         };
 
-        this.RegistrarAcoesDeEntrada();
+        this.RegistrarCallbacksDasEntradas();
     }
 
     private void OnEnable()
     {
-        for (int i = 0; i < this._acoesDeEntrada.Length; i++)
+        for (int i = 0; i < this._entradas.Length; i++)
         {
-            this._acoesDeEntrada[i].Enable();
+            this._entradas[i].Habilitar();
         }
     }
 
     private void OnDisable()
     {
-        for (int i = 0; i < this._acoesDeEntrada.Length; i++)
+        for (int i = 0; i < this._entradas.Length; i++)
         {
-            this._acoesDeEntrada[i].Disable();
+            this._entradas[i].Desabilitar();
         }
     }
 
-    private void RegistrarAcoesDeEntrada()
+    private void RegistrarCallbacksDasEntradas()
     {
-        // Interface
-        this._acaoDeEntradaDeAbrirMenu.performed += context => this._entradaDeAbrirOMenuDaInterface = true;
-        this._acaoDeEntradaDeAbrirMenu.canceled += context => this._entradaDeAbrirOMenuDaInterface = false;
-
-        this._acaoDeEntradaDeAbrirInventario.performed += context => this._entradaDeAbrirOInventarioDaInterface = true;
-        this._acaoDeEntradaDeAbrirInventario.canceled += context => this._entradaDeAbrirOInventarioDaInterface = false;
-
-        this._acaoDeEntradaDeAbrirDiario.performed += context => this._entradaDeAbrirODiarioDaInterface = true;
-        this._acaoDeEntradaDeAbrirDiario.canceled += context => this._entradaDeAbrirODiarioDaInterface = false;
-
-        this._acaoDeEntradaDeNavegar.performed += context => this._entradaDeNavegacaoDaInterface = context.ReadValue<Vector2>();
-        this._acaoDeEntradaDeNavegar.canceled += context => this._entradaDeNavegacaoDaInterface = Vector2.zero;
-
-        this._acaoDeEntradaDeSelecionar.performed += context => this._entradaDeSelecaoDaInterface = true;
-        this._acaoDeEntradaDeSelecionar.canceled += context => this._entradaDeSelecaoDaInterface = false;
-
-        // Jogador
-        this._acaoDeEntradaDeMover.performed += context => this._entradaDeMovimentoDoJogador = context.ReadValue<Vector2>();
-        this._acaoDeEntradaDeMover.canceled += context => this._entradaDeMovimentoDoJogador = Vector2.zero;
-
-        this._acaoDeEntradaDeCorrer.performed += context => this._entradaDeCorridaDoJogador = true;
-        this._acaoDeEntradaDeCorrer.canceled += context => this._entradaDeCorridaDoJogador = false;
-
-        this._acaoDeEntradaDeAgachar.performed += context => this._entradaDeAgacharDoJogador = true;
-        this._acaoDeEntradaDeAgachar.canceled += context => this._entradaDeAgacharDoJogador = false;
-
-        this._acaoDeEntradaDeInteragir.performed += context => this._entradaDeInteracaoDoJogador = true;
-        this._acaoDeEntradaDeInteragir.canceled += context => this._entradaDeInteracaoDoJogador = false;
-
-        this._acaoDeEntradaDeLanterna.performed += context => this._entradaDeLanternaDoJogador = true;
-        this._acaoDeEntradaDeLanterna.canceled += context => this._entradaDeLanternaDoJogador = false;
-
-        this._acaoDeEntradaDeVisao.performed += context => this._entradaDeVisaoDoJogador = context.ReadValue<Vector2>();
-        this._acaoDeEntradaDeVisao.canceled += context => this._entradaDeVisaoDoJogador = Vector2.zero;
-
-        this._acaoDeEntradaDeTrocarPOV.performed += context => this._entradaDeTrocaDePOVDoJogador = true;
-        this._acaoDeEntradaDeTrocarPOV.canceled += context => this._entradaDeTrocaDePOVDoJogador = false;
+        for (int i = 0; i < this._entradas.Length; i++)
+        {
+            this._entradas[i].RegistrarCallbacks();
+        }
     }
 }
